2365aca5c060f8171f9fca8da494525f1c6859a7
ericz <eczhang530@gmail.com>
2023-09-07 20:24:49 -0700

    use json format for vault output

--- toolbox/entrypoint.sh
+++ toolbox/entrypoint.sh
@@ -10,7 +10,8 @@ case $1 in
 
         echo "logging into vault..."
         vault write \
-            auth/kubernetes/login role=$TOOLBOX_VAULT_ROLE jwt=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token) >\
+            auth/kubernetes/login -format=json \
+            role=$TOOLBOX_VAULT_ROLE jwt=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token) >\
             /tmp/vault-token
         export VAULT_TOKEN=$(jq -r .auth.client_token /tmp/vault-token)
         cat /tmp/vault-token | grep -vw client_token
