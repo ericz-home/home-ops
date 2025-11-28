0b8f30ddbe311329a700d0504b8aa52b643e5c84
ericz <eczhang530@gmail.com>
2025-11-28 09:50:48 -0800

    update unifi role

--- tf/vault/pki/pki-unifi.tf
+++ tf/vault/pki/pki-unifi.tf
@@ -1,7 +1,7 @@
 resource "vault_pki_secret_backend_role" "unifi-role" {
-  backend = vault_mount.pki-lab-2025.path
+  backend = vault_mount.pki-lab-2026.path
   name    = "unifi-role"
-  max_ttl = vault_mount.pki-lab-2025.max_lease_ttl_seconds
+  max_ttl = vault_mount.pki-lab-2026.max_lease_ttl_seconds
   ttl     = 31536000 // 365d 
 
   allowed_domains    = ["unifi.home", "unifi"]
